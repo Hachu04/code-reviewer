c08017fcf00e0463bfe2da96ae0a0287f0b383e2
Hachu04 <chuvietha04@gmail.com>
2026-09-21 14:47:58 +0700

    Refactor environment variable handling and enhance API request headers for improved functionality

--- reviewer/main.py
+++ reviewer/main.py
@@ -3,9 +3,9 @@ import os
 import requests
 from google import genai
 
-owner = "hachu04"
-repo = "code-reviewer"  
-pr_number = "1"
+repo_full = os.environ.get("GITHUB_REPOSITORY", "hachu04/code-reviewer")
+owner, repo = repo_full.split("/")
+pr_number = os.environ.get("GITHUB_REF", "refs/pull/1/merge").split("/")[2]
 token = os.environ["GITHUB_TOKEN"]
 
 def get_pr_diff():
@@ -13,9 +13,11 @@ def get_pr_diff():
         f"https://api.github.com/repos/{owner}/{repo}/pulls/{pr_number}",
         headers={
             "Authorization": f"Bearer {token}",
-            "Accept": "application/vnd.github.v3.diff"
+            "Accept": "application/vnd.github.v3.diff",
+            "X-GitHub-Api-Version" : "2026-03-10"
         }
     )
+    response.raise_for_status()
     print(response.text)
     return response.text
 
@@ -36,9 +38,14 @@ def call_llm(prompt):
 def post_comment(comment):
     response = requests.post(
         f"https://api.github.com/repos/{owner}/{repo}/issues/{pr_number}/comments",
-        headers={"Authorization": f"Bearer {token}"},
+        headers={
+            "Authorization": f"Bearer {token}",
+            "Accept": "application/vnd.github+json",
+            "X-GitHub-Api-Version" : "2026-03-10"
+            },
         json={"body": comment}
     )
+    response.raise_for_status()
     print(response)
 
 if __name__ == "__main__":
