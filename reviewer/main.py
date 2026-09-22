021cbf11c324c11c2baf1e9a0caadb1f6b3fb76b
Hachu04 <chuvietha04@gmail.com>
2026-09-22 11:23:48 +0700

    Add repo context generation and update main.py to utilize context

--- reviewer/main.py
+++ reviewer/main.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 import requests
@@ -20,8 +21,52 @@ def get_pr_diff():
     response.raise_for_status()
     return response.text
 
-def build_prompt(diff):
-    prompt = f"You are a professional code reviewer, and your task is to look at pull request diff and check if there is any problem. Here is the diff: {diff}. I want you to return a structured list of problems: problem title, a brief description, where it occured, possible solution"
+def get_context(diff):
+    changed_functions = []
+    lines = diff.split('\n')
+    
+    for line in lines:
+        if (line.startswith('+') and not line.startswith('+++')) or \
+        (line.startswith('-') and not line.startswith('---')):
+            line = line.lstrip('+-')
+            if line.startswith('def'):
+                function = line.split('def ')[1].split('(')[0]
+                changed_functions.append(function)
+                
+    with open("repo_context.json", "r") as f:
+        context = json.load(f)
+    
+    reverse_index = {}
+    
+    for filepath, functions in context.items():
+        for function_name in functions:
+            reverse_index[function_name] = filepath
+    
+    function_sources = {}
+    
+    for function in changed_functions:
+        if function not in reverse_index:
+            continue
+        filepath = reverse_index[function]
+        start_line = context[filepath][function]['start_point']
+        end_line = context[filepath][function]['end_point']
+        
+        with open(filepath, "r") as f:
+            lines = f.readlines()
+        
+        source = "".join(lines[start_line:end_line])
+        function_sources[function] = {
+                "file": filepath,
+                "calls": context[filepath][function]["calls"],
+                "start_point": start_line,
+                "end_point": end_line,
+                "source": source
+            }
+        
+    return function_sources
+
+def build_prompt(diff, context):
+    prompt = f"You are a professional code reviewer, and your task is to look at pull request diff and check if there is any problem. Here is the diff: {diff}. Here is the context: {context} I want you to return a structured list of problems: problem title, a brief description, where it occured, possible solution"
     return prompt
 
 def call_llm(prompt):
@@ -47,6 +92,7 @@ def post_comment(comment):
 
 if __name__ == "__main__":
     diff = get_pr_diff()
-    prompt = build_prompt(diff)
+    context = get_context(diff)
+    prompt = build_prompt(diff, context)
     comment = call_llm(prompt)
     post_comment(comment)
---
+++ reviewer/tree-sitter.py
@@ -0,0 +1,58 @@
+import glob
+import json
+
+import tree_sitter_python as tspython
+from tree_sitter import Language, Parser
+
+PY_LANGUAGE = Language(tspython.language())
+
+parser = Parser(PY_LANGUAGE)
+
+def find_functions(node, functions=None):
+    if functions is None:
+        functions = {}
+    if node.type == 'function_definition':
+        name = node.child_by_field_name('name').text.decode('utf8')
+        functions[name] = {}
+        functions[name]["calls"] = find_calls_in_function(node, [])
+        functions[name]["start_point"] = node.start_point[0]
+        functions[name]["end_point"] = node.end_point[0]
+    for child in node.children:
+        find_functions(child, functions)
+    return functions
+
+def find_calls_in_function(node, calls=None):
+    if calls == None:
+        calls = []
+    if node.type == 'call':
+        name_node = node.child_by_field_name('function')
+        if name_node is not None:
+            name = name_node.text.decode('utf8')
+            calls.append(name)
+    for child in node.children:
+        find_calls_in_function(child, calls)
+    return calls
+
+def build_file_context(filepath):
+    with open(filepath, "rb") as f:
+        source = f.read()
+        
+    tree = parser.parse(source)
+    
+    return find_functions(tree.root_node)
+
+def build_repo_context(root_path=""):
+    files = glob.glob("**/*.py", recursive=True)
+    
+    repo_context = {}
+    
+    for file in files:
+        repo_context[file] = build_file_context(file)
+        
+    return repo_context
+
+print(build_repo_context())
+
+context = build_repo_context()
+with open("repo_context.json", "w") as f:
+    json.dump(context, f, indent=2)
